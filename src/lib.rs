b57421140b037cdaf2214013244aa3e0056d7c7b
M-Farag <mina.farag@icloud.com>
2023-03-17 14:55:51 +0200

    UPD Simplify functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub trait Aggregators {
-
-    fn max<T: std::cmp::PartialOrd> (&self,list_of_items:&Vec<T>) -> &T;
-    fn min<T: std::cmp::PartialOrd> (&self, list_of_items:&Vec<T>) -> &T;
+    fn max(&self) -> &usize;
+    fn min(&self) -> &usize;
+    fn new_list() -> Self;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
-use aggregator;
-pub mod Element;
+use aggregator::Aggregators;
+
+#[derive(Debug)]
+struct Numbers {
+    list_of_numbers:Vec<usize>,
+}
+
+impl Aggregators for Numbers{
+    fn new_list() -> Self
+    {
+        Self { list_of_numbers: Vec::new() }
+    }
+
+    fn min(&self) ->&usize {
+        let mut min = self.list_of_numbers.get(0).unwrap();
+        for num in &self.list_of_numbers {
+            if num < min {
+                min = num;
+            }
+        }
+        min
+    }
+
+    fn max(&self) ->&usize {
+        let mut max = self.list_of_numbers.get(0).unwrap();
+        for num in &self.list_of_numbers {
+            if num > max {
+                max = num;
+            }
+        }
+        max
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut x:Numbers = Numbers::new_list();
+    x.list_of_numbers.push(1);
+    x.list_of_numbers.push(6);
+    x.list_of_numbers.push(3);
+
+    println!("Min value is: {}",x.min());
+    println!("Max value is: {}",x.max());
 }
